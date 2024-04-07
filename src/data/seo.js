cd55d352e77f9ad1da973d29d7ecaa1743946062
Stelian Cioarec <cioarecstelian@gmai.com>
2024-04-07 23:45:04 +0300

    Yes.

--- src/data/seo.js
+++ src/data/seo.js
@@ -1,1 +1,5 @@
 export const url = 'https://www.enciclopedianinjago.ro/'
+
+export const defaultTitle = 'Enciclopedia Ninjago'
+
+export const defaultDescription = 'Enciclopedia Ninjago este un site dedicat serialului animat Ninjago: Maeștrii Spinjitzu. Aici vei găsi informații despre personaje, locații, arme, vehicule, episoade, sezoane și multe altele.'
---
+++ src/data/series.js
@@ -0,0 +1,10 @@
+export const ascensiuneaDragonilor = {
+    title: 'NINJAGO: Ascensiunea Dragonilor',
+    desc: 'Cu ajutorul puterilor Ninja recent descoperite, doi adolescenți din lumi diferite apără dragonii de cei care vor să le folosească forța vitală în slujba răului.',
+    releaseDate: '2023 - 2024',
+    seasons: 2,
+    episodes: 20,
+    rating: "7+",
+    genres: ['Animație', 'Acțiune', 'Aventură'],
+    seoImage: '/images/seo/ascensiunea-dragonilor.jpg'
+}
